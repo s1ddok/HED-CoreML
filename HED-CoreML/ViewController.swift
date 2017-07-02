76cd4bc8fde2fe2bbd59f1ee4ba6ca5fe82ea29e
s1ddok <siddok@gmail.com>
2017-07-02 22:33:40 +0300

    Initial Commit

---
+++ HED-CoreML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HED-CoreML
+//
+//  Created by Andrey Volodin on 02.07.17.
+//  Copyright © 2017 s1ddok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
